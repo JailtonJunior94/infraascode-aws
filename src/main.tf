9c570d2bc13973f343cde7fd2f096bbfc59e53a3
jailton.junior <jailton.junior@iterative.com.br>
2021-09-13 19:00:41 -0300

    Criação de novos componentes

--- src/main.tf
+++ src/main.tf
@@ -1,11 +1,35 @@
 terraform {
+  required_version = "1.0.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.58.0"
+    }
+  }
+
   backend "s3" {
     bucket = "storageterraformjj"
-    key    = "finance-infra"
+    key    = "finance-infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
+  profile = var.profile
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "storageterraformjj"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    "Owner" = "Jailton Junior"
+  }
 }
