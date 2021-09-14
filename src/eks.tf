6a5df73bbdf946e26debc03fc172fb4484680f0a
jailton.junior <jailton.junior@iterative.com.br>
2021-09-14 09:38:51 -0300

    Criação da infraestrutura

--- src/eks.tf
+++ src/eks.tf
@@ -21,13 +21,6 @@ module "eks" {
       asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
   ]
 }
 
